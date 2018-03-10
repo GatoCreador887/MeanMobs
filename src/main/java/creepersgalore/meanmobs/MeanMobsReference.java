2c734edb5965a00c7158528813a0d347cf95cc4b
GatoCreador887 <ajolego@gmail.com>
2018-03-10 09:16:45 -0500

    Updated version value

--- src/main/java/creepersgalore/meanmobs/MeanMobsReference.java
+++ src/main/java/creepersgalore/meanmobs/MeanMobsReference.java
@@ -4,6 +4,6 @@ public class MeanMobsReference {
 	
 	public static final String ID = "meanmobs";
 	public static final String NAME = "MeanMobs";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	
 }
